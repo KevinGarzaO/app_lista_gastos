4bfbfb1c1ab4ff0d636b6c10ea20fc8dba60d09f
Kevin Garza <kevin_4569@msn.com>
2022-01-22 13:19:01 -0600

    Creando Iniciar Sesion

--- src/components/InicioSesion.jsx
+++ src/components/InicioSesion.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Header, Titulo, ContenedorHeader } from './../elementos/Header';
+import Boton from './../elementos/Boton'
+import { Formulario, Input, ContenedorBoton } from './../elementos/ElementosDeFormulario'
+import { ReactComponent as SvgLogin } from './../images/login.svg';
+import styled from 'styled-components';
+
+const Svg = styled(SvgLogin)`
+    width: 100%;
+    max-height: 12.5rem; /*100 px*/
+    margin-bottom: 1.25rem; /*2 px*/
+`;
 
 const InicioSesion = () => {
-    return ( 
-        <h1>Inicio Sesion</h1>
-     );
+    return (
+        <>
+            <Helmet>
+                <title>Iniciar Sesion</title>
+            </Helmet>
+
+            <Header>
+                <ContenedorHeader>
+                    <Titulo>Iniciar Sesion</Titulo>
+                    <div>
+                        <Boton to="/crear-cuenta">Registrarse</Boton>
+                    </div>
+                </ContenedorHeader>
+            </Header>
+
+            <Formulario>
+                <Svg />
+                <Input type="email" name="email" placeholder='Correo Electrónico' />
+                <Input type="password" name="password" placeholder='Contraseña' />
+                
+
+                <ContenedorBoton>
+                    <Boton as="button" primario type='submit'>Iniciar Sesion</Boton>
+                </ContenedorBoton>
+
+
+            </Formulario>
+
+        </>
+    );
 }
- 
+
 export default InicioSesion;
