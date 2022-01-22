83df0a90cdb03655811cc54c1397c92347711fb3
Kevin Garza <kevin_4569@msn.com>
2022-01-22 11:02:13 -0600

    Estructura Inicial y Rutas de la app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import WebFont from 'webfontloader'
+import Contenedor from './elementos/Contenedor'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import EditarGastos from './components/EditarGastos'
+import GastosPorCategoria from './components/GastosPorCategoria'
+import InicioSesion from './components/InicioSesion'
+import ListaDeGastos from './components/ListaDeGastos'
+import RegistroUsuarios from './components/RegistroUsuarios'
 
 WebFont.load({
   google: {
@@ -11,7 +18,20 @@ WebFont.load({
 });
 
 const Index = () => {
-  return ( <App /> );
+  return ( 
+    <BrowserRouter>
+      <Contenedor>
+        <Routes>
+         <Route path="/iniciar-sesion" element={<InicioSesion />} />
+         <Route path="/crear-cuenta" element={<RegistroUsuarios />} />
+         <Route path="/categorias" element={<GastosPorCategoria />} />
+         <Route path="/Lista" element={<ListaDeGastos />} />
+         <Route path="/editar/:id" element={<EditarGastos />} />
+         <Route path="/" element={<App />} />
+        </Routes> 
+      </Contenedor>
+    </BrowserRouter>
+  );
 }
  
 ReactDOM.render(<Index />, document.getElementById('root'));
