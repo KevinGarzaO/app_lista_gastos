65d6fb51ff5cceb382eb25d28d2db9efd3fe4189
Kevin Garza <kevin_4569@msn.com>
2022-01-24 21:00:47 -0600

    Inisio de sesion con firebase

--- src/components/InicioSesion.jsx
+++ src/components/InicioSesion.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Helmet } from 'react-helmet';
 import { Header, Titulo, ContenedorHeader } from './../elementos/Header';
 import Boton from './../elementos/Boton'
 import { Formulario, Input, ContenedorBoton } from './../elementos/ElementosDeFormulario'
 import { ReactComponent as SvgLogin } from './../images/login.svg';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import {auth} from './../firebase/firebaseConfig'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import Alerta from '../elementos/Alerta';
+
 
 const Svg = styled(SvgLogin)`
     width: 100%;
@@ -13,6 +18,76 @@ const Svg = styled(SvgLogin)`
 `;
 
 const InicioSesion = () => {
+    const navigate = useNavigate();
+    const[correo, setCorreo] = useState('');
+    const[password, setPassword] = useState('');
+    const[estadoAlerta, setEstadoAlerta] = useState(false);
+    const[alerta, setAlerta] = useState({});
+
+    const handleChange = (e) =>{
+        if(e.target.name === 'email'){
+            setCorreo(e.target.value);
+        }else if(e.target.name === 'password'){
+            setPassword(e.target.value);
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setEstadoAlerta(false);
+        setAlerta({});
+
+        if(correo === '' || password === ''){
+            setEstadoAlerta(true);
+            setAlerta({
+                tipo: "error",
+                mensaje: "Por favor llenar todos los datos"
+            });
+          
+            return;
+        }
+
+        //Comprobamos del lado del cliente que el correo sea valido.
+        const expresionRegular = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+        if(!expresionRegular.test(correo)){
+            setEstadoAlerta(true);
+            setAlerta({
+                tipo: "error",
+                mensaje: "Por favor ingresa un correo electronico valido"
+            });
+            return;
+        }
+
+       
+        try{
+            await signInWithEmailAndPassword(auth, correo, password)
+            navigate('/');
+        }catch(error){
+            console.log(error);
+            let mensaje;
+            switch(error.code){
+                case "auth/wrong-password":
+                    mensaje = "La contraseña no es correcta"
+                    break;
+                case 'auth/user-not-found':
+                    mensaje = 'No existe usuario favor de volver a intentar.'
+                    break;
+                // case 'auth/invalid-email':
+                //     mensaje = 'El correo electrónico no es válido.'
+                //     break;
+                default:
+                    mensaje = 'Hubo un error al intentar crear la cuenta.'
+                    break;
+            
+            }
+            setEstadoAlerta(true);
+            setAlerta({
+                tipo: "error",
+                mensaje: mensaje
+            });
+        }
+    }
+
     return (
         <>
             <Helmet>
@@ -28,10 +103,10 @@ const InicioSesion = () => {
                 </ContenedorHeader>
             </Header>
 
-            <Formulario>
+            <Formulario onSubmit={handleSubmit}>
                 <Svg />
-                <Input type="email" name="email" placeholder='Correo Electrónico' />
-                <Input type="password" name="password" placeholder='Contraseña' />
+                <Input type="email" name="email" placeholder='Correo Electrónico' value={correo} onChange={handleChange}/>
+                <Input type="password" name="password" placeholder='Contraseña' value={password} onChange={handleChange}/>
                 
 
                 <ContenedorBoton>
@@ -40,6 +115,8 @@ const InicioSesion = () => {
 
 
             </Formulario>
+
+            <Alerta tipo={alerta.tipo} mensaje={alerta.mensaje} estadoAlerta={estadoAlerta} setEstadoAlerta={setEstadoAlerta}/>
 
         </>
     );
