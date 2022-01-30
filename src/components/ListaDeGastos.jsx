665d0f780c9dfb97a7b85924c011940a78fd9b74
Kevin Garza <kevin.garza@crediclub.com>
2022-01-30 14:06:18 -0600

    estructura de lista de gastos.

--- src/components/ListaDeGastos.jsx
+++ src/components/ListaDeGastos.jsx
@@ -5,7 +5,7 @@ import BtnRegresar from '../elementos/BtnRegresar';
 import { useAuth } from '../contexts/AuthContext';
 import BarraTotalGastado from './BarraTotalGastado';
 import useObtenerGastos from '../hooks/useObtenerGastos';
-import  {
+import {
     Lista,
     ElementoLista,
     ListaDeCategorias,
@@ -20,19 +20,37 @@ import  {
     ContenedorBotonCentral,
     ContenedorSubtitulo,
     Subtitulo
-}  from '../elementos/ElementosDeLista';
+} from '../elementos/ElementosDeLista';
 import IconoCategoria from './../elementos/IconoCategoria'
 import convertirAMoneda from './../functions/convertirAMoneda'
-import {ReactComponent as IconoEditar} from './../images/editar.svg'
-import {ReactComponent as IconoBorrar} from './../images/borrar.svg'
+import { ReactComponent as IconoEditar } from './../images/editar.svg'
+import { ReactComponent as IconoBorrar } from './../images/borrar.svg'
 import { Link } from 'react-router-dom';
 import Boton from './../elementos/Boton'
+import {format, fromUnixTime} from 'date-fns'
+import {es} from 'date-fns/locale'
 
 const ListaDeGastos = () => {
-    const {usuario} = useAuth();
+    const { usuario } = useAuth();
     const [gastos] = useObtenerGastos();
-   
-    
+
+    const formatearFecha = (fecha) =>{
+      return (format(fromUnixTime(fecha), "dd 'de' MMMM 'de' yyyy", {locale: es}));
+    }
+
+    const fechaEsIgual = (gastos, index, gasto) =>{
+        if(index !== 0){
+            const fechaActual = formatearFecha(gasto.fecha);
+            const fechaGastoAnterior = formatearFecha(gastos[index -1].fecha);
+           
+            if(fechaActual === fechaGastoAnterior){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
     return (
         <>
             <Helmet>
@@ -45,30 +63,33 @@ const ListaDeGastos = () => {
             </Header>
 
             <Lista>
-                {gastos.map((gasto) =>{
-                    return(
-                        <ElementoLista key={gasto.id}>
-                            <Categoria>
-                                <IconoCategoria nombre={gasto.categoria} />
-                                {gasto.categoria}
-                            </Categoria>
+                {gastos.map((gasto, index) => {
+                    return (
+                        <div key={gasto.id}>
+                            {!fechaEsIgual(gastos, index, gasto) && <Fecha>{formatearFecha(gasto.fecha)}</Fecha>}
+                            <ElementoLista key={gasto.id}>
+                                <Categoria>
+                                    <IconoCategoria nombre={gasto.categoria} />
+                                    {gasto.categoria}
+                                </Categoria>
 
-                            <Descripcion>
-                                {gasto.descripcion}
-                            </Descripcion>
-                            <Valor>
-                                {convertirAMoneda(gasto.cantidad)}
-                            </Valor>
+                                <Descripcion>
+                                    {gasto.descripcion}
+                                </Descripcion>
+                                <Valor>
+                                    {convertirAMoneda(gasto.cantidad)}
+                                </Valor>
 
-                            <ContenedorBotones>
-                                <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
-                                    <IconoEditar />
-                                </BotonAccion>
-                                <BotonAccion>
-                                    <IconoBorrar />
-                                </BotonAccion>
-                            </ContenedorBotones>
-                        </ElementoLista>
+                                <ContenedorBotones>
+                                    <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
+                                        <IconoEditar />
+                                    </BotonAccion>
+                                    <BotonAccion>
+                                        <IconoBorrar />
+                                    </BotonAccion>
+                                </ContenedorBotones>
+                            </ElementoLista>
+                        </ div>
                     );
                 })}
 
@@ -79,13 +100,13 @@ const ListaDeGastos = () => {
                 </ContenedorBotonCentral>
 
 
-                {gastos.length === 0  && 
-                <ContenedorSubtitulo>
-                    <Subtitulo>
-                        No hay gastos por mostrar
-                        <Boton as={Link} to="/">Agregar gasto</Boton>
-                    </Subtitulo>
-                </ContenedorSubtitulo>
+                {gastos.length === 0 &&
+                    <ContenedorSubtitulo>
+                        <Subtitulo>
+                            No hay gastos por mostrar
+                            <Boton as={Link} to="/">Agregar gasto</Boton>
+                        </Subtitulo>
+                    </ContenedorSubtitulo>
                 }
             </Lista>
 
